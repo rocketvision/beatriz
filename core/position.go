ce5f81ec1b1e516cac5faa7fcac25cdd7bbf7ef4
Lord <10253775+lewdum@users.noreply.github.com>
2021-06-28 23:54:25 -0300

    Position methods

--- core/position.go
+++ core/position.go
@@ -1,6 +1,12 @@
 package core
 
+import "fmt"
+
 type Position struct {
 	Line   int
 	Column int
+}
+
+func (p Position) String() string {
+	return fmt.Sprintf("Line %v, Col %v", p.Line, p.Column)
 }
